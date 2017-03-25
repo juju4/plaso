a37863c49840bed91a92c9957904d3dc4935ada8
juju4 <juju4@users.noreply.github.com>
2017-03-25 19:24:49 -0400

    add parser+formatter for exim4 mainlog/paniclog

--- plaso/parsers/__init__.py
+++ plaso/parsers/__init__.py
@@ -13,6 +13,7 @@ from plaso.parsers import custom_destinations
 from plaso.parsers import docker
 from plaso.parsers import dpkg
 from plaso.parsers import esedb
+from plaso.parsers import exim
 from plaso.parsers import filestat
 from plaso.parsers import firefox_cache
 from plaso.parsers import hachoir
